e4eeb2c78cfc0fb3bb22693d68ac50e35c3bccbe
nagorealbisu <156786495+nagorealbisu@users.noreply.github.com>
2025-07-23 15:39:13 +0200

    Create nn_crypto_context_secure.cpp

---
+++ src/encrypted_nn/nn_crypto_context_secure.cpp
@@ -0,0 +1,80 @@
+/*
+  Simple examples for CKKS
+ */
+
+#define PROFILE
+
+#include "openfhe.h"
+#include <random>
+#include <cmath>
+#include <iostream>
+
+#include "globals.hpp"
+
+extern "C" {
+    #include "nn.h"
+    #include "train.h"
+}
+
+using namespace lbcrypto;
+
+KeyPair<DCRTPoly> init_crypto_context_ckks_train(CryptoContext<DCRTPoly> &cc, nn_t *nn, uint32_t &numSlots){
+    CCParams<CryptoContextCKKSRNS> parameters;
+
+    parameters.SetSecretKeyDist(UNIFORM_TERNARY);
+    parameters.SetSecurityLevel(HEStd_128_classic);
+    //parameters.SetSecurityLevel(HEStd_NotSet);  // jostailuzkoa --> HEStd_128_classic gutxienez
+    //parameters.SetRingDim(1 << 12); // jostailuzkoa. HEStd_128_classic --> 1<<17 gutxienez
+
+    #if NATIVEINT == 128
+        parameters.SetScalingTechnique(FIXEDAUTO);
+        parameters.SetScalingModSize(78);
+        parameters.SetFirstModSize(89);
+    #else
+        parameters.SetScalingTechnique(FLEXIBLEAUTO);
+        parameters.SetScalingModSize(59);
+        parameters.SetFirstModSize(60);
+    #endif
+
+    parameters.SetBatchSize(numSlots); // neurona kopuruarena arabera --> XOR: 2
+
+    std::vector<uint32_t> levelBudget = {4, 4}; // edo {2,2}. {1,1} errorea bootstrapping egiterakoan
+    std::vector<uint32_t> bsgsDim = {0, 0};
+
+    uint32_t levelsAfterBootstrap = 20;
+    depth = levelsAfterBootstrap + FHECKKSRNS::GetBootstrapDepth(levelBudget, UNIFORM_TERNARY);
+    //std::cout << "\nDepth: " << depth << std::endl;
+    parameters.SetMultiplicativeDepth(depth);
+
+    cc = GenCryptoContext(parameters);
+
+    cc->Enable(PKE);
+    cc->Enable(KEYSWITCH);
+    cc->Enable(LEVELEDSHE);
+    cc->Enable(ADVANCEDSHE);
+    cc->Enable(FHE);
+
+    KeyPair<DCRTPoly> keys = cc->KeyGen();
+    cc->EvalMultKeysGen(keys.secretKey);
+    cc->EvalSumKeyGen(keys.secretKey);
+
+    //numSlots = cc->GetRingDimension() / 2;
+
+    // EvalMerge erabiltzeko beharrezkoak diren RotationKey-ak sortu
+    std::vector<int32_t> indexList;
+
+    for(int i = 1; i < (int)numSlots; i++){ // ALDATUTA
+        indexList.push_back(i);
+        indexList.push_back(-i);
+    }
+    cc->EvalAtIndexKeyGen(keys.secretKey, indexList);
+
+    // Bootstrapping keys
+    cc->EvalBootstrapSetup(levelBudget, bsgsDim, numSlots);
+    cc->EvalBootstrapKeyGen(keys.secretKey, numSlots);
+
+    std::cout << "\nMultDepth: " << depth << std::endl;
+
+    return keys;
+}
+
